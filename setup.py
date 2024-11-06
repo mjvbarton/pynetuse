a579c0d155195bfbc0202e9d7f71e941b3eaabc8
mjvbarton <matej.8.barton@gmail.com>
2024-11-06 19:39:49 +0100

    Updates package metadata

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pynetuse",               # Name of the package
-    version="1.0.0",                # Version of the package
+    name="pynetuse",               
+    version="1.0.0",       
     author="Matej Barton",
     author_email="info@mjvbarton.cz",
     description="A python wrapper for Windows net use",
-    long_description=open("README.md").read(),  # Optional: add README.md content
-    long_description_content_type="text/markdown",  # Type of README (e.g., Markdown)
-    url="https://github.com/username/mypackage",  # URL to the project
-    packages=find_packages(),        # Automatically find package directories
-    install_requires=[               # List of dependencies
-        "numpy>=1.18.0",             # Example dependency
+    long_description=open("README.md").read(), 
+    long_description_content_type="text/markdown",  
+    url="https://github.com/mjvbarton/pynetuse",  
+    packages=find_packages(),
+    install_requires=[               
+        "numpy>=1.18.0",
         "requests>=2.20.0"
     ],
-    classifiers=[                    # Optional: metadata for searchability on PyPI
+    classifiers=[             
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Windows",
     ],
     python_requires=">=3.6",
 )
